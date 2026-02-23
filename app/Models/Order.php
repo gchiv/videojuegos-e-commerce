ba8a9c0e9e5aba42967bcbcd814e462ddcb1e161
gchiv <gchividal@gmail.com>
2026-02-23 17:52:28 -0600

    edite estos 2 archivos ya que se encargan de guardar la compra

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total',
+        'status'
+    ];
+
+    public function items(){
+        return $this->hasMany(OrderItem::class);
+    }
 }
