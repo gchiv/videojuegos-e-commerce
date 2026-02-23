0fc5e353203080ec75ef751c214823a2df1a8c8b
gchiv <gchividal@gmail.com>
2026-02-23 09:44:54 -0600

    Agregue el achivo AuthController que verifica las credenciales y genera el token y agregue las rutas en api.php

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))){
+            return response()->json([
+                'message' => 'Credencciales Incorrectas'
+            ], 401);
+        }
+
+            $user = User::where('email', $request->email)->firstOrFail();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Hola' . $user->name,
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ]);
+        }
+
+        public function logout(){
+            auth()->user()->tokens()->delete();
+
+            return response()->json([
+                'message' => 'Sesión cerrada correctamente'
+            ]);
+        }
+    }
+
