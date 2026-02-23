2176168a21562ac06d6fbf3ec490dee4a3dd0ace
gchiv <gchividal@gmail.com>
2026-02-23 17:44:10 -0600

    agregue la ruta de OrderController en Routes/api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\OrderController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
@@ -15,4 +16,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/cart', [CartController::class, 'addToCart']);
     Route::get('/cart', [CartController::class, 'viewCart']);
+
+    Route::post('/checkout', [OrderController::class, 'checkout']);
 });
+
+
